7eb055c71beba5eb09a603a08f1d7ff8f9e1e36f
tomgrove <tomkelgrove@yahoo.co.uk>
2015-06-05 09:00:22 +0100

    more refactoring

--- scheme/scheme.cpp
+++ scheme/scheme.cpp
@@ -8,9 +8,10 @@
 #include "boost\any.hpp"
 #include "schemetypes.h"
 #include "collectable.h"
+#include "context.h"
 
-static bool gTrace = false;
-static bool gVerboseGC = false;
+bool gTrace = false;
+bool gVerboseGC = false;
 
 template<typename T>
 struct Maybe
@@ -76,80 +77,6 @@ static std::string print(Item item);
 struct Context;
 void eval(Item item, Context* context, std::function<void(Item)> k);
 void gc(Context*);
-
-struct Context : public Collectable<Context> 
-{
-	std::map< Symbol, Item >	mBindings;
-	Context*					mOuter;
-
-	Context()
-		: mOuter(nullptr)
-		, mBindings()
-	{}
-
-	Context(Context* context)
-		: mOuter(context)
-	{}
-
-	Context( Item variables, CellRef params, Context* outer)
-		: mOuter(outer)
-	{
-		// variable is symbol: any number of arguments; arguments bound to symbol
-		if (variables.type() == eSymbol)
-		{
-			mBindings[ boost::any_cast<Symbol>(variables) ] = params;
-		}
-		else
-		{ 
-			// variable is list
-			assert( variables.type() == eCell);
-			CellRef variablelist = boost::any_cast<CellRef>( variables );
-			while (variablelist) {
-				if (gTrace)
-				{
-					std::stringstream sstream;
-					sstream << "Binding: " << print(variablelist->mCar) << " = " << print(params->mCar) << std::endl;
-					puts(sstream.str().c_str());
-				}
-				mBindings[ boost::any_cast<Symbol>(variablelist->mCar) ] = params->mCar;
-				 
-				if (variablelist->mCdr.type() == eCell)
-				{
-					variablelist = boost::any_cast<CellRef>( variablelist->mCdr );
-					params = boost::any_cast<CellRef>( params->mCdr);
-				}
-				// pattern matching
-				else
-				{
-					assert(variablelist->mCdr.type() == eSymbol);
-					mBindings[ boost::any_cast<Symbol>( variablelist->mCdr) ] = params->mCdr;
-					return;
-				}
-			}
-		}
-	}
-
-	Item Lookup( Symbol symbol )
-	{
-		if (mBindings.find(symbol) == mBindings.end())
-		{
-			if (mOuter)
-			{
-				return mOuter->Lookup(symbol);
-			}
-			else
-			{
-				return Unspecified();
-			}
-		}
-		return mBindings[symbol];
-	}
-
-	void Set(uint32_t symbol, Item value)
-	{
-		mBindings[symbol] = value;
-	}
-};
 
 const static uint32_t cMaxCells = 1000000;
 const static uint32_t cMaxContexts = 1000;
